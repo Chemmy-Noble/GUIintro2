9020ed139ab97bda4927139f80a50b33671462de
chiemerie.noble <chiemerie.noble@AMS-GYM-EDV1-05.gym.ams.local>
2020-03-02 18:50:02 +0100

    taschenrechner

---
+++ src/guiintro2/MyFirstWindow.java
@@ -0,0 +1,105 @@
+package guiintro2;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+public class MyFirstWindow {
+
+	protected Shell shell;
+	private Text EZtf;
+	private Text ergebnis;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			MyFirstWindow window = new MyFirstWindow();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		
+		EZtf = new Text(shell, SWT.BORDER);
+		EZtf.setBounds(10, 53, 76, 21);
+		
+		ergebnis = new Text(shell, SWT.BORDER);
+		ergebnis.setText("0");
+		ergebnis.setBounds(10, 123, 76, 21);
+		
+		Label lblZahl = new Label(shell, SWT.NONE);
+		lblZahl.setBounds(10, 32, 55, 15);
+		lblZahl.setText("Zahl");
+		
+		Label lblErgebnis = new Label(shell, SWT.NONE);
+		lblErgebnis.setBounds(10, 102, 55, 15);
+		lblErgebnis.setText("Ergebnis");
+		
+		Button button = new Button(shell, SWT.NONE);
+		button.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				String text = getErgebnis().getText();
+				int ergebnis = Integer.parseInt(text);
+				String text2 = getText().getText();
+				int zahl2 = Integer.parseInt(text2);
+				ergebnis = ergebnis + zahl2;
+				//
+				getErgebnis().setText(Integer.toString(ergebnis));
+			}
+		});
+		button.setBounds(298, 53, 75, 25);
+		button.setText("+");
+		
+		Button button_1 = new Button(shell, SWT.NONE);
+		button_1.setBounds(298, 84, 75, 25);
+		button_1.setText("-");
+		
+		Button button_2 = new Button(shell, SWT.NONE);
+		button_2.setBounds(298, 119, 75, 25);
+		button_2.setText("*");
+		
+		Button button_3 = new Button(shell, SWT.NONE);
+		button_3.setBounds(298, 150, 75, 25);
+		button_3.setText("/");
+
+	}
+
+	public Text getErgebnis() {
+		return ergebnis;
+	}
+	public Text getText() {
+		return EZtf;
+	}
+}
--- src/guiintro2/MySecondWindow.java
+++ src/guiintro2/MySecondWindow.java
@@ -1,4 +1,5 @@
 package guiintro2;
+
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Button;
@@ -7,14 +8,16 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.TouchListener;
+import org.eclipse.swt.events.TouchEvent;
 
 public class MySecondWindow {
 
-	protected Shell shell;
-	private Text Vornametf;
+	protected Shell ok;
 
 	/**
 	 * Launch the application.
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -32,9 +35,7 @@ public class MySecondWindow {
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		while (!ok.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -45,32 +46,18 @@ public class MySecondWindow {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("SWT Application");
-		
-		Button btnMyButton = new Button(shell, SWT.NONE);
-		btnMyButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				System.out.println("My Button wurde gedrückt");
-			}
-		});
-		btnMyButton.setBounds(174, 68, 75, 25);
-		btnMyButton.setText("My Button");
-		
-		Label lblVorname = new Label(shell, SWT.NONE);
-		lblVorname.setBounds(115, 138, 55, 15);
-		lblVorname.setText("Vorname");
-		
-		Vornametf = new Text(shell, SWT.BORDER);
-		Vornametf.setBounds(173, 138, 76, 21);
+		ok = new Shell();
+	}
 
+	public Text getEZtf() {
+		return EZtf;
 	}
-	public Text getVtf() {
-		return vtf;
+
+	public Text getZTtf() {
+		return ZTtf;
 	}
-	public Text getVornametf() {
-		return Vornametf;
+
+	public Button getErgebnis() {
+		return ergebnis;
 	}
 }
