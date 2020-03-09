6dfc6e20cfb3026d7726121b173a52b64939e934
chiemerie.noble <chiemerie.noble@AMS-GYM-EDV1-05.gym.ams.local>
2020-03-09 18:51:52 +0100

    ja

--- src/guiintro2/MyFirstWindow.java
+++ src/guiintro2/MyFirstWindow.java
@@ -14,6 +14,7 @@ public class MyFirstWindow {
 	protected Shell shell;
 	private Text EZtf;
 	private Text ergebnis;
+	private Text text_1;
 
 	/**
 	 * Launch the application.
@@ -71,65 +72,162 @@ public class MyFirstWindow {
 		button.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				String text = getErgebnis().getText();
-				int ergebnis = Integer.parseInt(text);
+				String text = Textfeld().getText();
+				int text_1 = Integer.parseInt(text);
 				String text2 = getText().getText();
 				int zahl2 = Integer.parseInt(text2);
-				ergebnis = ergebnis + zahl2;
+				text_1 = text_1 + zahl2;
 				//
-				getErgebnis().setText(Integer.toString(ergebnis));
+				getErgebnis().setText(Integer.toString(text_1));
 			}
 		});
-		button.setBounds(298, 53, 75, 25);
+		button.setBounds(349, 166, 75, 25);
 		button.setText("+");
 
 		Button button_1 = new Button(shell, SWT.NONE);
 		button_1.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				String text = getErgebnis().getText();
-				int ergebnis = Integer.parseInt(text);
+				String text = Textfeld().getText();
+				int text_1 = Integer.parseInt(text);
 				String text2 = getText().getText();
 				int zahl2 = Integer.parseInt(text2);
-				ergebnis = ergebnis - zahl2;
+				text_1 = text_1 - zahl2;
 				//
-				getErgebnis().setText(Integer.toString(ergebnis));
+				getErgebnis().setText(Integer.toString(text_1));
 			}
 		});
-		button_1.setBounds(298, 84, 75, 25);
+		button_1.setBounds(349, 135, 75, 25);
 		button_1.setText("-");
 
 		Button button_2 = new Button(shell, SWT.NONE);
 		button_2.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				String text = getErgebnis().getText();
-				int ergebnis = Integer.parseInt(text);
+				String text = Textfeld().getText();
+				int text_1 = Integer.parseInt(text);
 				String text2 = getText().getText();
 				int zahl2 = Integer.parseInt(text2);
-				ergebnis = ergebnis * zahl2;
+				text_1 = text_1 * zahl2;
 				//
-				getErgebnis().setText(Integer.toString(ergebnis));
+				getErgebnis().setText(Integer.toString(text_1));
 			}
 		});
-		button_2.setBounds(298, 119, 75, 25);
+		button_2.setBounds(349, 97, 75, 25);
 		button_2.setText("*");
 
 		Button button_3 = new Button(shell, SWT.NONE);
 		button_3.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				String text = getErgebnis().getText();
-				int ergebnis = Integer.parseInt(text);
+				String text = Textfeld().getText();
+				int text_1 = Integer.parseInt(text);
 				String text2 = getText().getText();
 				int zahl2 = Integer.parseInt(text2);
-				ergebnis = ergebnis / zahl2;
+				text_1 = text_1 / zahl2;
 				//
-				getErgebnis().setText(Integer.toString(ergebnis));
+				getErgebnis().setText(Integer.toString(text_1));
 			}
 		});
-		button_3.setBounds(298, 150, 75, 25);
+		button_3.setBounds(349, 58, 75, 25);
 		button_3.setText("/");
+		
+		Button button_4 = new Button(shell, SWT.NONE);
+		button_4.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				String text = Textfeld().getText();
+				int text_1 = Integer.parseInt(text);
+			}
+		});
+		button_4.setBounds(106, 166, 75, 25);
+		button_4.setText("0");
+		
+		
+		Button button_5 = new Button(shell, SWT.NONE);
+		button_5.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		button_5.setBounds(106, 133, 75, 25);
+		button_5.setText("1");
+		
+		Button button_6 = new Button(shell, SWT.NONE);
+		button_6.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		button_6.setBounds(187, 133, 75, 25);
+		button_6.setText("2");
+		
+		Button button_7 = new Button(shell, SWT.NONE);
+		button_7.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		button_7.setBounds(268, 133, 75, 25);
+		button_7.setText("3");
+		
+		Button button_8 = new Button(shell, SWT.NONE);
+		button_8.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		button_8.setBounds(106, 97, 75, 25);
+		button_8.setText("4");
+		
+		Button button_9 = new Button(shell, SWT.NONE);
+		button_9.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		button_9.setBounds(187, 97, 75, 25);
+		button_9.setText("5");
+		
+		Button button_10 = new Button(shell, SWT.NONE);
+		button_10.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		button_10.setBounds(268, 97, 75, 25);
+		button_10.setText("6");
+		
+		Button button_11 = new Button(shell, SWT.NONE);
+		button_11.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		button_11.setBounds(106, 58, 75, 25);
+		button_11.setText("7");
+		
+		Button button_12 = new Button(shell, SWT.NONE);
+		button_12.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		button_12.setBounds(187, 58, 75, 25);
+		button_12.setText("8");
+		
+		Button button_13 = new Button(shell, SWT.NONE);
+		button_13.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		button_13.setBounds(268, 58, 75, 25);
+		button_13.setText("9");
+		
+		text_1 = new Text(shell, SWT.BORDER);
+		text_1.setText("0");
+		text_1.setBounds(105, 10, 319, 37);
 
 	}
 
@@ -139,5 +237,8 @@ public class MyFirstWindow {
 
 	public Text getText() {
 		return EZtf;
+	}
+	public Text Textfeld() {
+		return text_1;
 	}
 }
